7077af02db99d70e22f65e2a147398e228c55e51
Pit Wenkin <45786226+PitWenkin@users.noreply.github.com>
2019-04-20 12:48:25 +0200

    Version 1.1
    Added variables for warning and critical status
    Default values are 1% of aps down for warning, 10% down for critical

--- check_wc.sh
+++ check_wc.sh
@@ -2,37 +2,45 @@
 
 #Makes use of https://github.com/lausser/check_nwc_health to list number of working accesspoints
 #Written by Pit Wenkin
+#Version 1.1
+# - Added variables for warning and critical status
 
 accesspoints="0"
 community=""
 hostname=""
 port="161"
-
+warning="1"
+critical="10"
 
 usage()
 {
-        echo "usage: ./check_wc -h [hostname] -c [community] -p [port] -a [accesspoints]"
+        echo "usage: ./check_wc -h [hostname] -c [community] -p [port] -a [accesspoints] -W [warning] -C [critical]"
         echo "options:"
         echo "            -h [hostname]"
         echo "            -c [SNMPv2 community name]	(ex: public)"
         echo "            -p [port]			(default is 161)"
         echo "            -a [accesspoints]		Number of accesspoints which should be online"
+        echo "            -W [warning]  Percentage of accesspoints which have to be down to return a warning message"
+        echo "            -C [critical] Percentage of accesspoints which have to be down to return a critical message"
         echo ""
-        echo "examples: ./check_wc -c private -p 1234 -h nas.intranet"  
+        echo "examples: ./check_wc -c private -p 1234 -h nas.intranet"
+        echo "          ./check_wc -c private -p 1234 -h nas.intranet -W 5 -C 10"
         exit 3
 }
 
-while getopts 2:a:c:p:h: OPTNAME; do
+while getopts 2:a:c:p:h:C:W: OPTNAME; do
         case "$OPTNAME" in
         p)      port="$OPTARG";;
         h)      hostname="$OPTARG";;
         c)      community="$OPTARG";;
         a)      accesspoints="$OPTARG";;
+        C)      critical="$OPTARG";;
+        W)      warning="$OPTARG";;
         esac
 done
 if [ "$hostname" = "" ] || [ "$community" = "" ] ; then
     usage
-else 
+else
         output=`/usr/lib/nagios/plugins/check_nwc_health --hostname $hostname --port $port --community $community --mode list-interfaces | grep -o '[0-9]\{6\} ap[0-9]\{1,3\}s0' | wc -l`
         apdown=`expr $output - $accesspoints`
         apup=`expr $accesspoints + $apdown`
@@ -41,13 +49,19 @@ else
                 hundred=`expr 100 '*' "$apdown"`
                 percent=`expr $hundred / $accesspoints`
                 percent=${percent%.*}
-                if [ "$percent" -gt "10" ] ; then
+                if [ "$percent" -gt "$critical" ] ; then
                         echo "CRITCAL - $apdown accesspoints are down / $apup accesspoints are up"
                         exit 2
+                else
+                        if [ "$percent" -gt "$warning" ] ; then
+                                echo "WARNING - $apdown accesspoints are down / $apup accesspoints are up"
+                                exit 1
+                        fi
                 fi
-                echo "WARNING - $apdown accesspoints are down / $apup accesspoints are up"
-                exit 1
+                echo "OK - $apdown accesspoints are down / $apup accesspoints are up"
+                exit 0
         fi
+
         if [ "$apdown" = "0" ] ; then
                 echo "OK - $accesspoints accesspoints are up"
                 exit 0
